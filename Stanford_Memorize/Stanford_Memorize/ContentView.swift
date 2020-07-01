104b02f94a8213cb28f4a8bfe100a1dbd9ba22fa
Michiel Leunens <michiel.leunens@gmail.com>
2020-07-01 11:08:58 +0200

    initial commit

---
+++ Stanford_Memorize/Stanford_Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stanford_Memorize
+//
+//  Created by Michiel Leunens on 01/07/2020.
+//  Copyright © 2020 Leunes Media. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
